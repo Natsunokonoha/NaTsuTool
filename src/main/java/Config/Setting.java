6428fb7bbc6c8d427f2ebb1936d1d22567ff0dba
Natsunokonoha <897791398@qq.com>
2024-10-11 00:13:48 +0800

    新增userExtend和userOption的Api

--- src/main/java/Config/Setting.java
+++ src/main/java/Config/Setting.java
@@ -15,6 +15,8 @@ public class Setting {
             return "6165a4a2828e5ac54c215da37b83a71d";
         }
         public static String userData() { return "9bf7b5cbd258358e0de2188b324b6272"; }
+        public static String userExtend() { return "73f95003558d73b3392337a7a86459e4"; }
+        public static String userOption() { return "dc877d2c8fbb9305317ed79e71b48e56"; }
         public static String userCharacterList() { return "16768656af052a9a3cd612e3a612a219"; } //旅行伙伴all
         public static String userItemList() { return "e168a0bcd17a57ce9ce302b4ad9c4713";} //用户物品
 
@@ -46,5 +48,9 @@ public class Setting {
     }
     public static String clientId() {
         return "A63E01E1191";
+    }
+
+    public static class Others {
+        public static int Thread() { return 5; }
     }
 }
