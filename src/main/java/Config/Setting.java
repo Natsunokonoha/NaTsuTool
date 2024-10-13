3fea5318ff5f89c0f53c680fba5fa549516d795f
natsunokonoha <41945685+Natsunokonoha@users.noreply.github.com>
2024-10-13 21:27:23 +0800

    Update Setting.java
    fix project
    
    Signed-off-by: natsunokonoha <41945685+Natsunokonoha@users.noreply.github.com>

--- src/main/java/Config/Setting.java
+++ src/main/java/Config/Setting.java
@@ -17,8 +17,9 @@ public class Setting {
         public static String userData() { return "9bf7b5cbd258358e0de2188b324b6272"; }
         public static String userExtend() { return "73f95003558d73b3392337a7a86459e4"; }
         public static String userOption() { return "dc877d2c8fbb9305317ed79e71b48e56"; }
-        public static String userCharacterList() { return "16768656af052a9a3cd612e3a612a219"; } //旅行伙伴展示
+        public static String userCharacterList() { return "16768656af052a9a3cd612e3a612a219"; } //旅行伙伴updata
         public static String userItemList() { return "e168a0bcd17a57ce9ce302b4ad9c4713";} //用户物品
+        public static String userMapList() { return "05ee513488df5591935157cc6877399e"; }
 
     }
 
