dc071e70139ec96931827629a8f653b2575e4b85
Natsunokonoha <897791398@qq.com>
2024-10-15 12:59:56 +0800

    将Api改为未解密APi

--- src/main/java/Config/Setting.java
+++ src/main/java/Config/Setting.java
@@ -9,12 +9,12 @@ public class Setting {
             return "GetUserPreviewApiMaimaiChn";
         }
         public static String juan() {
-            return "949d55efb2ea032f511b97aa153473f7";
+            return "GetGameChargeApiMaimaiChn";
         }
         public static String logout() {
-            return "6165a4a2828e5ac54c215da37b83a71d";
+            return "UserLogoutApiMaimaiChn";
         }
-        public static String userData() { return "9bf7b5cbd258358e0de2188b324b6272"; }
+        public static String userData() { return "GetUserDataApiMaimaiChn"; }
         public static String userExtend() { return "73f95003558d73b3392337a7a86459e4"; }
         public static String userOption() { return "dc877d2c8fbb9305317ed79e71b48e56"; }
         public static String userCharacterList() { return "16768656af052a9a3cd612e3a612a219"; } //旅行伙伴updata
