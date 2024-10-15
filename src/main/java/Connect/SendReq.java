e435fc18654a08ab00b2e162dac845e147474a90
Natsunokonoha <897791398@qq.com>
2024-10-15 12:48:08 +0800

    添加对于Api的加密

--- src/main/java/Connect/SendReq.java
+++ src/main/java/Connect/SendReq.java
@@ -4,27 +4,27 @@ import Config.Setting;
 
 public class SendReq {
     public static String Login(String Userid, Long currentTimestamp){
-        return Dispose.build(Userid, Setting.Api.Login(), RequestBodyBuilder.login(Userid, currentTimestamp));
+        return Dispose.build(Userid, Dispose.obfuscate(Setting.Api.Login()), RequestBodyBuilder.login(Userid, currentTimestamp));
     }
     public static String Ratting(String Userid){
-        return Dispose.build(Userid, Setting.Api.ratting(), RequestBodyBuilder.ratting(Userid));
+        return Dispose.build(Userid, Dispose.obfuscate(Setting.Api.ratting()), RequestBodyBuilder.ratting(Userid));
     }
     public static String Track(String Userid,int Ratting){
-        return Dispose.build(Userid, Setting.Api.juan(), RequestBodyBuilder.juan(Userid, Ratting));
+        return Dispose.build(Userid, Dispose.obfuscate(Setting.Api.juan()), RequestBodyBuilder.juan(Userid, Ratting));
     }
     public static String Logout(String Userid, Long currentTimestamp){
-        return Dispose.build(Userid, Setting.Api.logout(), RequestBodyBuilder.logout(Userid, currentTimestamp));
+        return Dispose.build(Userid, Dispose.obfuscate(Setting.Api.logout()), RequestBodyBuilder.logout(Userid, currentTimestamp));
     }
     public static String userData(String Userid){
-        return Dispose.build(Userid, Setting.Api.userData(), RequestBodyBuilder.userData(Userid));
+        return Dispose.build(Userid, Dispose.obfuscate(Setting.Api.userData()), RequestBodyBuilder.userData(Userid));
     }
     public static String userExtend(String Userid){
-        return Dispose.build(Userid, Setting.Api.userExtend(), RequestBodyBuilder.userExtend(Userid));
+        return Dispose.build(Userid, Dispose.obfuscate(Setting.Api.userExtend()), RequestBodyBuilder.userExtend(Userid));
     }
     public static String userOption(String Userid){
-        return Dispose.build(Userid, Setting.Api.userOption(), RequestBodyBuilder.userOption(Userid));
+        return Dispose.build(Userid, Dispose.obfuscate(Setting.Api.userOption()), RequestBodyBuilder.userOption(Userid));
     }
     public static String userMapList(String Userid){
-        return Dispose.build(Userid, Setting.Api.userMapList(), RequestBodyBuilder.userMapList(Userid));
+        return Dispose.build(Userid, Dispose.obfuscate(Setting.Api.userMapList()), RequestBodyBuilder.userMapList(Userid));
     }
 }
